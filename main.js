d88f26ccd39e03604f6122501b9bb7b043e97172
Guido De Vita <guidito.ito@gmail.com>
2021-09-13 16:20:14 -0300

    fixed responsive for buttons fixed form responsive reused inPesos fixed button to submit TODO: fix favorites

--- main.js
+++ main.js
@@ -1,12 +1,63 @@
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
+function clearTokenDisplay() {
+    for (let token of tokens) {
+        token.displayed = false;
+    }
+}
+function printToken(token) {
+    $("#tblPrice").append(`
+                <tr>
+                <td>${token.name}</td>
+                <td>U$S ${token.priceStr()}</td>
+                <td>AR$ ${token.inPesosStr()}</td>
+                <td><a class="fav" id="fav-${token.name}"><i class="fas fa-star"></i></a></td>
+                </tr>
+                `);
+    //avoid repeating
+    token.displayed = true;
+}
+
+class Favorites {
+    constructor(){
+        this.tokens = [];
+        this.restore();
+    }
+    add(token){
+        if (!this.tokens.includes(token)) {
+            this.tokens.append(token.toString());
+            this.save();
+        }
+    }
+    remove(token){
+        if (this.tokens.includes(token)) {
+            const index = this.tokens.indexOf(token);
+            if (index > -1) {
+                this.tokens.splice(index, 1);
+            }
+            this.save();
+        }
+    }
+    save(){
+        localStorage.clear();
+        localStorage.setItem("tokens",JSON.stringify(this.tokens));
+    }
+    restore(){
+        const restored = localStorage.getItem("tokens");
+        if (restored) {
+            this.tokens = JSON.parse(restored);
+        }
+    }
+}
+
 
 
 class Token{
     constructor(token) {
         this.name = token.name;
         this.price = parseFloat(token.price);
+        this.displayed = false;
     }
     inPesos(){
         return this.price * 185;
@@ -25,6 +76,8 @@ token2 = new Token({name:"eth", price: 3000});
 token3 = new Token({name:"zil", price:0.13});
 
 let tokens = [token1, token2, token3];
+
+let favs = new Favorites();
 
 let prices = document.getElementById("tblPrice");
 //Example without jQuery
@@ -53,33 +106,28 @@ $(function() {
         while (parent.firstChild) {
             parent.firstChild.remove()
         }
+        clearTokenDisplay();
     });
 
-    $("#loadBtn").on('click', function(){
+    $("#loadBtn").click(function(){
         for (let token of tokens) {
-            $("#tblPrice").append(`
-                <tr>
-                <td>${token.name}</td>
-                <td>U$S ${token.priceStr()}</td>
-                <td>AR$ ${token.inPesosStr()}</td>
-                <td><i class="fas fa-star"></i></td>
-                </tr>
-            `);
+            if (!token.displayed){
+                printToken(token);
+            }
         }
     });
-    $("#searchForm").on('submit', function(e) {
+    $(".navbar-search").submit(function(e) {
         e.preventDefault();
         let formValues = new FormData(e.target);
         for (let token of tokens){
-
-            if (formValues.get("coin") === token.name) {
-                $("#tblPrice").append(`
-                     <td>${token.name}</td>
-                     <td>U$S ${token.priceStr()}</td>
-                     <td>AR$ ${token.inPesosStr()}</td>
-                     <td><i class="fas fa-star"></i></td>
-            `);
-            }
+            printToken(token);
         }
+    });
+    $(".fav").on('click', function() {
+        //TODO click not working. dynamically generated html ?
+        alert("clicked");
+        let fav = $(this).attr(id).replace('fav-','');
+        favs.add(fav);
+        $(this).innerHTML.css('color','gold');
     })
 });
