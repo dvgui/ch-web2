65005b31e4ed047e67a6c8918ba02d0e60c3bdd3
Guido De Vita <guidito.ito@gmail.com>
2021-09-20 19:49:49 -0300

    -added support for metamask. now connects and handles switching accounts. TODO: add wallet tokens to table

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ function printToken(token) {
     token.displayed = true;
 
     $(`#fav-${token.name}`).on('click', function() {
-        let fav = this.id.replace('fav-','');
         if (favs.isFavorite(token.name)) {
             favs.remove(token.name);
             $(`#fav-${token.name} .fas`).css('color','grey');
@@ -39,6 +38,22 @@ function printToken(token) {
         else {
             favs.add(token.name);
             $(`#fav-${token.name} .fas`).css('color','gold');
+        }
+    });
+}
+
+function metaConnect(response){
+    chainId = parseInt(response.chainId);
+    signer = provider.getSigner();
+
+    //Fetch default account
+    provider.listAccounts().then(async (accounts) => {
+        wallet_address = accounts[0];
+        if (wallet_address) {
+            console.log("Reconnected account:", wallet_address);
+            let balance = await provider.getBalance(await signer.getAddress())
+            console.log("ETH Balance: " + ethers.utils.formatEther(balance));
+            $("#walletConnect button").removeClass('btn-danger').addClass('btn-success');
         }
     });
 }
@@ -88,10 +103,12 @@ class Favorites {
 
 
 class Token{
-    constructor(name, price) {
+    constructor(name, price, contract) {
         this.name = name;
         this.price = parseFloat(price);
+        this.contract = new Contract(contract.network, contract.address)
         this.displayed = false;
+        this.balance = 0;
     }
     inPesos(){
         return this.price * 185;
@@ -107,20 +124,48 @@ class Token{
     }
 }
 
+class Contract {
+    constructor (network, address) {
+        this.network = network;
+        this.address = address;
+    }
+}
+
 
 // INIT
 
-const token1 = new Token("btc", 40000);
-const token2 = new Token("eth", 3000);
-const token3 = new Token("zil", 0.13);
+const token1 = new Token("btc", 40000,{
+    network :'eth',
+    address : '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
+});
+const token2 = new Token("eth", 3000,{
+    network : 'eth',
+    address : ''
+});
+const token3 = new Token("zil", 0.13,{
+    network : 'zil',
+    address : ''
+});
 
 let tokens = [token1, token2, token3];
+let wallet_address, chainId, signer;
+
 
 const favs = new Favorites();
 
+// A Web3Provider wraps a standard Web3 provider, which is
+// what Metamask injects as window.ethereum into each page
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// The Metamask plugin also allows signing transactions to
+// send ether and pay to change state within the blockchain.
+// For this, you need the account signer...
 
 
 let prices = document.getElementById("tblPrice");
+
+
+
 
 $(function() {
     $("#resetBtn").on('click', function (){
@@ -131,7 +176,7 @@ $(function() {
         clearTokenDisplay();
     });
 
-    $("#loadBtn").click(function(){
+    $("#loadBtn").on('click', function(){
         for (let token of tokens) {
             if (!token.displayed){
                 printToken(token);
@@ -151,4 +196,67 @@ $(function() {
     });
 
 
+    $('body').ready(() => {
+        $("#loadBtn").trigger('click');
+
+    });
+    //initial connect
+    $('#walletConnect').on('click', async () => {
+        signer = provider.getSigner()
+        await provider.send("eth_requestAccounts", []);
+        wallet_address = await signer.getAddress()
+        // log
+        console.log("Connected account:", wallet_address);
+        let balance = await provider.getBalance(await signer.getAddress())
+        console.log("ETH Balance: " + ethers.utils.formatEther(balance));
+        //$("#walletDisconnect").removeClass('d-none');
+        $("#walletConnect button").removeClass('btn-danger').addClass('btn-success');
+        // TODO: detect when wallet is available & change icon to green.
+        // TODO: warn when network is not ethereum
+        // TODO: list tokens
+
+
+    });
+    $('#walletDisconnect').on('click', async () =>{
+        // TODO desconect from metamask
+        // ethereum.on('accountsChanged', handler: (accounts: Array<string>) => void);
+
+        $('#walletDisconnect').hide();
+    });
+
+
+
+
+});
+// detect if metamask is available
+//log
+if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed!');
+}
+else {
+    console.log('Metamask is not installed');
+    $('#walletDisconnect').hide();
+}
+
+window.ethereum.on('chainChanged', () => {
+    window.location.reload();
+});
+window.ethereum.on('disconnect', () => {
+    window.location.reload();
+});
+/*
+TODO: añadir evento a conexion en vez de al click del boton. para que se dispare cuando metamask engancha.
+*/
+
+//reconnect
+window.ethereum.addListener('connect', async (response) => {
+    metaConnect(response);
+});
+
+window.ethereum.addListener('disconnect', () => {
+    console.log("Wallet disconnected");
+    $("#walletConnect button").removeClass('btn-success').addClass('btn-info');
+});
+window.ethereum.addListener('accountsChanged', async (response) => {
+    metaConnect(response);
 });
